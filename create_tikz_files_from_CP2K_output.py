f3529008b5b03656bebeb3efdc6288136d42a4d3
JWilhelm <jan.wilhelm@ur.de>
2023-08-26 09:18:15 +0200

    preparation for printing only bands around fermi level, not yet working

--- create_tikz_files_from_CP2K_output.py
+++ create_tikz_files_from_CP2K_output.py
@@ -3,9 +3,6 @@ import os
 def create_directory_if_not_exists(directory_name):
     if not os.path.exists(directory_name):
         os.mkdir(directory_name)
-        print(f"Directory '{directory_name}' created.")
-    else:
-        print(f"Directory '{directory_name}' already exists.")
 
 def get_number_of_bands(filename):
     found = False
@@ -36,7 +33,13 @@ def get_number_of_kpoints(filename):
 
     return nkp, nkp_special
 
-def read_bandstructure_and_write_tikz_data(filename, fname_write, nkp, nkp_special, n_bands, e_fermi):
+def any_number_in_interval(arr, a, b):
+    for num in arr:
+        if a <= num <= b:
+            return True
+    return False
+
+def read_bandstructure_and_write_tikz_data(filename, fname_write, nkp, nkp_special, n_bands, e_fermi, energy_window):
 
     bandstructure = [[0.0] *  n_bands for _ in range(nkp)]
     xkp = [0.0] * nkp
@@ -62,36 +65,22 @@ def read_bandstructure_and_write_tikz_data(filename, fname_write, nkp, nkp_speci
                 band_index = int(line_split[0])
                 band_energy = float(line_split[1])
                 bandstructure[ikp-1][band_index-1] = band_energy
-                print("ikp", ikp, "band_index", band_index, "energy =", bandstructure[ikp-1][band_index-1])
-
 
     for band_index in range(n_bands):
         with open(fname_write+str(band_index)+".dat", 'w') as f:
             for ikp in range(nkp):
+               e1 = e_fermi - energy_window/2
+               e2 = e_fermi + energy_window/2
+               is_band_around_e_fermi = any_number_in_interval(bandstructure[:][band_index], e1, e2)
                f.write(str(abskp[ikp]) + ' ' + str(bandstructure[ikp][band_index]) + '\n')
 
 # names and running the program
 data_dir = "data"
+bandstructure_file_cp2k = "bandstructure_SCF.bs"
 create_directory_if_not_exists(data_dir)
-nkp, nkp_special = get_number_of_kpoints("bandstructure_SCF.bs")
-n_bands = get_number_of_bands("bandstructure_SCF.bs")
+nkp, nkp_special = get_number_of_kpoints(bandstructure_file_cp2k)
+n_bands = get_number_of_bands(bandstructure_file_cp2k)
 e_fermi = -1.0
-read_bandstructure_and_write_tikz_data("bandstructure_SCF.bs", data_dir+"/band_SCF_", \
-                                       nkp, nkp_special, n_bands, e_fermi)
-print("Number of kpoints is ", nkp)
-print("Number of bands is ", n_bands)
-
-
-
-## Write the atoms to an XYZ file
-#filename = 'nat_'+str(len(atoms))+'_parameters_'+\
-#str(n_vertical_double_layers_pyr)+'_'+\
-#str(n_vertical_double_layers_cub)+'_'+\
-#str(n_xy_cube)+'_'+\
-#str(thickness_pyr )+'_'+\
-#str(thickness_cub)+'_'+\
-#str(dist_frontier_atoms)+\
-#'.xyz'
-#write_xyz_file(filename, atoms)
-#
-#print('XYZ file "{}" has been created.'.format(filename))
+energy_window = 10.0
+read_bandstructure_and_write_tikz_data(bandstructure_file_cp2k, data_dir+"/band_SCF_", \
+                                       nkp, nkp_special, n_bands, e_fermi, energy_window)
